f9e5d1c5d4c7296b27bf010e8dedca4acbb3375f
Jonas Dixon <135274411+dixonjonas@users.noreply.github.com>
2025-05-06 09:23:59 +0200

    Add files via upload

---
+++ CodeLlama-34b/clone.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone https://huggingface.co/codellama/CodeLlama-34b-hf
+git clone https://huggingface.co/codellama/CodeLlama-34b-Instruct-hf
+git clone https://huggingface.co/codellama/CodeLlama-34b-Python-hf
+git clone https://huggingface.co/Phind/Phind-CodeLlama-34B-Python-v1
+git clone https://huggingface.co/Phind/Phind-CodeLlama-34B-v2
+git clone https://huggingface.co/WizardLMTeam/WizardCoder-Python-34B-V1.0
+git clone https://huggingface.co/patched-codes/patched-coder-34b
