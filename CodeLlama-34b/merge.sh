4733c001c1fc150acdb6a9a94d5008732713ccc3
Jonas Dixon <135274411+dixonjonas@users.noreply.github.com>
2025-05-08 13:18:05 +0200

    Update merge.sh

--- CodeLlama-34b/merge.sh
+++ CodeLlama-34b/merge.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 mergekit-yaml /mnt/data/thesis/CodeLlama-34b/SOUP.yml /mnt/data/CodeLlama-34b-SOUP --cuda --low-cpu-memory
+mergekit-yaml /mnt/data/thesis/CodeLlama-34b/TA_1.yml /mnt/data/CodeLlama-34b-TA --cuda --low-cpu-memory
