c4dc3c6e226e6d17448b0223903a46cac996b813
Jonas Dixon <135274411+dixonjonas@users.noreply.github.com>
2025-05-16 10:03:19 +0200

    Update SLERP_merge.sh

--- CodeLlama-34b/SLERP_merge.sh
+++ CodeLlama-34b/SLERP_merge.sh
@@ -2,6 +2,6 @@
 
 mergekit-yaml /mnt/data/thesis/CodeLlama-34b/SLERP_1-2.yml /mnt/data/SLERP_1-2 --cuda --low-cpu-memory
 mergekit-yaml /mnt/data/thesis/CodeLlama-34b/SLERP_3-4.yml /mnt/data/SLERP_3-4 --cuda --low-cpu-memory
-mergekit-yaml /mnt/data/thesis/CodeLlama-34b/SLERP_5-6.yml /mnt/data/SLERP_5-6 --cuda --low-cpu-memory
 mergekit-yaml /mnt/data/thesis/CodeLlama-34b/SLERP_1-2-3-4.yml /mnt/data/SLERP_1-2-3-4 --cuda --low-cpu-memory
+mergekit-yaml /mnt/data/thesis/CodeLlama-34b/SLERP_5-6.yml /mnt/data/SLERP_5-6 --cuda --low-cpu-memory
 mergekit-yaml /mnt/data/thesis/CodeLlama-34b/SLERP_final.yml /mnt/data/CodeLlama-34b-SLERP --cuda --low-cpu-memory
